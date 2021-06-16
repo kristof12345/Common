2a9122e7469ab345a568bed09ebe122b66d0b3e2
Kristof Papp <pappkristof1@gmail.com>
2021-06-16 08:29:11 +0200

    Common tests

---
+++ Tests/Models/WebModelTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Common.Application;
+using Common.Web;
+using Xunit;
+
+namespace Common.Tests.Models
+{
+    public class WebModelTests
+    {
+        [Fact]
+        public void BorderToStringTest()
+        {
+            var border = new Border();
+            Assert.Equal("0px 0px 0px 0px", border.ToString());
+
+            border = new Border("1px");
+            Assert.Equal("1px 1px 1px 1px", border.ToString());
+
+            border = new Border("1px", "2px");
+            Assert.Equal("2px 1px 2px 1px", border.ToString());
+
+            border = new Border("1px", "2px", "3px", "4px");
+            Assert.Equal("1px 2px 3px 4px", border.ToString());
+        }
+
+        [Fact]
+        public void ChartDataTest()
+        {
+            var data = new List<ChartData>
+            {
+                new ChartData { Id = "a", Data = 5, Color = "b", Label = "c" },
+                new ChartData { Id = "1", Data = 10, Color = "2", Label = "3" }
+            };
+
+            Assert.Equal("3", data.GetById("1").Label);
+            Assert.Equal(5, data.GetById("a").Data);
+            Assert.Equal("b", data.GetById("a").Color);
+        }
+    }
+}
