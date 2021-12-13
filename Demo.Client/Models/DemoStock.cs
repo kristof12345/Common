81fb4b75e9f8e27515d5b189762df598247dbeee
Kristof Papp <pappkristof1@gmail.com>
2021-12-13 18:32:32 +0100

    Server side demo

---
+++ Demo.Client/Models/DemoStock.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Common.Application;
+
+namespace Demo.Client
+{
+    public class DemoStock
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Exchange { get; set; }
+
+        public string Currency { get; set; }
+
+        public string Country { get; set; }
+
+        public string Sector { get; set; }
+
+        public string Industry { get; set; }
+
+        public long MarketCapitalization { get; set; }
+
+        public long NumberOfShares { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal EPS { get; set; }
+
+        public long EBITDA { get; set; }
+
+        public decimal PayoutRatio { get; set; }
+
+        public decimal ReturnOnEquity { get; set; }
+
+        public decimal Dividend { get; set; }
+
+        public decimal ForwardDividend { get; set; }
+
+        public decimal Beta { get; set; }
+
+        public decimal FairValue { get; set; }
+
+        public decimal ExpectedReturn { get; set; }
+
+        public List<StockPrice> Prices { get; set; }
+    }
+
+    public class StockPrice : IStockPrice
+    {
+        public DateTime Date { get; set; }
+
+        public decimal Open { get; set; }
+
+        public decimal High { get; set; }
+
+        public decimal Low { get; set; }
+
+        public decimal Close { get; set; }
+
+        public long Volume { get; set; }
+    }
+}
---
+++ Demo.Server/Startup.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Blazored.LocalStorage;
+using Syncfusion.Blazor;
+using Microsoft.Extensions.Hosting;
+using Common.Web;
+using Common.Application;
+using System.Net.Http;
+using System;
+using Blazored.SessionStorage;
+using Plk.Blazor.DragDrop;
+
+namespace Demo.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5000/") });
+
+            services.AddSignalR(e => { e.MaximumReceiveMessageSize = 102400000; });
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+
+            services.AddBlazorDragDrop();
+            services.AddBlazoredLocalStorage();
+            services.AddBlazoredSessionStorage();
+            services.AddSyncfusionBlazor();
+
+            services.AddHttpClient();
+
+            services.AddScoped<ToastService>();
+            services.AddScoped<ThemeService>();
+            services.AddScoped<DataService<DemoStock>>();
+        }
+
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/Host");
+            });
+        }
+    }
+}
