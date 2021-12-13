c9b777964bb7d08d285c295bdfcc66c34824eb2f
Kristof Papp <pappkristof1@gmail.com>
2021-12-13 18:32:44 +0100

    Chart rendering

---
+++ Frontend/Services/LoginService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using Blazored.SessionStorage;
+using Common.Application;
+
+namespace Common.Web
+{
+    public class LoginService
+    {
+        public event Func<Task> LoginEvent;
+
+        private readonly HttpClient Client;
+
+        private readonly ILocalStorageService LocalStorage;
+
+        private readonly ISessionStorageService SessionStorage;
+
+        public AppUser User { get; private set; }
+
+        public LoginService(HttpClient client, ILocalStorageService localStorage, ISessionStorageService sessionStorage)
+        {
+            Client = client;
+            LocalStorage = localStorage;
+            SessionStorage = sessionStorage;
+        }
+
+        public async Task<Response> Login(LoginRequest request)
+        {
+            try
+            {
+                var response = await Client.PutAsJsonAsync("login", request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await SaveUser(await response.Content.ReadAsAsync<AppUser>());
+                }
+
+                return await response.Content.ReadAsAsync<Response>();
+            }
+            catch (Exception)
+            {
+                return new Response("Unable to connect to server.");
+            }
+        }
+
+        public async Task<Response> SaveUser(AppUser user)
+        {
+            User = user;
+            await SessionStorage.SetItemAsync("user", User);
+            Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", User.Token);
+            LoginEvent?.Invoke();
+            return Response.Success;
+        }
+
+        public async Task Logout()
+        {
+            User = null;
+            await SessionStorage.ClearAsync();
+            await LocalStorage.ClearAsync();
+            LoginEvent?.Invoke();
+        }
+    }
+}
+
