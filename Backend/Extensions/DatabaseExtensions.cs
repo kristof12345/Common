9304761a62d59b0fb8f445132f59dfe298fd01b5
Kristof Papp <pappkristof1@gmail.com>
2021-12-15 09:22:02 +0100

    Update DB extensions

--- Backend/Extensions/DatabaseExtensions.cs
+++ Backend/Extensions/DatabaseExtensions.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Common.Application;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace Common.Backend
 {
@@ -18,28 +19,63 @@ namespace Common.Backend
             return await entities.FirstOrDefaultAsync(e => e.Id.Equals(id));
         }
 
-        public static async Task<T> InsertAsync<T>(this DbSet<T> entities, T entity, DbContext context) where T : class
+        public static async Task<T> InsertAsync<T>(this DbSet<T> entities, T entity) where T : class
         {
-            await entities.AddAsync(entity);
-            await context.SaveChangesAsync();
+            entities.Add(entity);
+            await entities.SaveAsync();
             return entity;
         }
 
-        public static async Task InsertAsync<T>(this DbSet<T> entities, IEnumerable<T> entity, DbContext context) where T : class
+        public static async Task InsertAsync<T>(this DbSet<T> entities, IEnumerable<T> entity) where T : class
         {
-            await entities.AddRangeAsync(entity);
-            await context.SaveChangesAsync();
+            entities.AddRange(entity);
+            await entities.SaveAsync();
         }
 
-        public static async Task<T> DeleteAsync<T, R>(this IQueryable<T> entities, R id, DbContext context) where T : class, IEntity<R>
+        public static async Task<IEntity<R>> UpdateAsync<T, R>(this DbSet<T> entities, IEntity<R> entity) where T : class, IEntity<R>
+        {
+            var original = await entities.AsTracking().FindAsync(entity.Id);
+            if (original != null)
+            {
+                entities.Remove(original);
+                entities.Add((T)entity);
+                await entities.SaveAsync();
+            }
+            return entity;
+        }
+
+        public static async Task<T> DeleteAsync<T, R>(this DbSet<T> entities, R id) where T : class, IEntity<R>
         {
             var entity = await entities.AsTracking().FindAsync(id);
             if (entity != null)
             {
-                context.Remove(entity);
-                await context.SaveChangesAsync();
+                entities.Remove(entity);
+                await entities.SaveAsync();
             }
             return entity;
+        }
+
+        public static void DiscardChanges(this DbContext context)
+        {
+            foreach (var entry in context.ChangeTracker.Entries())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Modified:
+                    case EntityState.Deleted:
+                        entry.State = EntityState.Modified;
+                        entry.State = EntityState.Unchanged;
+                        break;
+                    case EntityState.Added:
+                        entry.State = EntityState.Detached;
+                        break;
+                }
+            }
+        }
+
+        private static async Task SaveAsync<T>(this DbSet<T> entities) where T : class
+        {
+            await entities.GetService<ICurrentDbContext>().Context.SaveChangesAsync();
         }
     }
 }
--- Tests/Database/DatabaseExtensionTests.cs
+++ Tests/Database/DatabaseExtensionTests.cs
@@ -19,7 +19,7 @@ namespace Common.Tests.Database
             var context = new DatabaseContext(new DbContextOptionsBuilder<DatabaseContext>().UseInMemoryDatabase("DemoDB1").Options);
 
             // Act
-            await context.Entities.InsertAsync(new Entity { Id="1", Content="a" }, context);
+            await context.Entities.InsertAsync(new Entity { Id="1", Content="a" });
 
             // Assert
             Assert.Equal(1, context.Entities.Count());
@@ -32,7 +32,7 @@ namespace Common.Tests.Database
             var context = new DatabaseContext(new DbContextOptionsBuilder<DatabaseContext>().UseInMemoryDatabase("DemoDB2").Options);
 
             // Act
-            await context.Entities.InsertAsync(new List<Entity> { new Entity { Id = "1", Content = "a" }, new Entity { Id = "2", Content = "b" } }, context);
+            await context.Entities.InsertAsync(new List<Entity> { new Entity { Id = "1", Content = "a" }, new Entity { Id = "2", Content = "b" } });
 
             // Assert
             Assert.Equal(2, context.Entities.Count());
@@ -43,10 +43,10 @@ namespace Common.Tests.Database
         {
             // Arrange
             var context = new DatabaseContext(new DbContextOptionsBuilder<DatabaseContext>().UseInMemoryDatabase("DemoDB3").Options);
+            await context.Entities.InsertAsync(new List<Entity> { new Entity { Id = "1", Content = "a" }, new Entity { Id = "2", Content = "b" } });
 
             // Act
-            await context.Entities.InsertAsync(new List<Entity> { new Entity { Id = "1", Content = "a" }, new Entity { Id = "2", Content = "b" } }, context);
-            await context.Entities.DeleteAsync("2", context);
+            await context.Entities.DeleteAsync("2");
 
             // Assert
             Assert.Equal(1, context.Entities.Count());
@@ -57,10 +57,9 @@ namespace Common.Tests.Database
         {
             // Arrange
             var context = new DatabaseContext(new DbContextOptionsBuilder<DatabaseContext>().UseInMemoryDatabase("DemoDB4").Options);
+            await context.Entities.InsertAsync(new List<Entity> { new Entity { Id = "1", Content = "a" }, new Entity { Id = "2", Content = "b" } });
 
             // Act
-            await context.Entities.InsertAsync(new List<Entity> { new Entity { Id = "1", Content = "a" }, new Entity { Id = "2", Content = "b" } }, context);
-
             var list = await context.Entities.OrderByWithDirection(new Ordering<Entity>(e => e.Content, false)).ToListAsync();
 
             // Assert
@@ -74,16 +73,49 @@ namespace Common.Tests.Database
         {
             // Arrange
             var context = new DatabaseContext(new DbContextOptionsBuilder<DatabaseContext>().UseInMemoryDatabase("DemoDB5").Options);
+            await context.Entities.InsertAsync(new List<Entity> { new Entity { Id = "1", Content = "a" }, new Entity { Id = "2", Content = "b" } });
 
             // Act
-            await context.Entities.InsertAsync(new List<Entity> { new Entity { Id = "1", Content = "a" }, new Entity { Id = "2", Content = "b" } }, context);
-
             var list = await context.Entities.OrderByWithDirection(new Ordering<Entity>(e => e.Content, true)).ToListAsync();
 
             // Assert
             Assert.Equal(2, list.Count);
             Assert.Equal("2", list.First().Id);
             Assert.Equal("1", list.Last().Id);
+        }
+
+
+        [Fact]
+        public async Task UpdateTest()
+        {
+            // Arrange
+            var context = new DatabaseContext(new DbContextOptionsBuilder<DatabaseContext>().UseInMemoryDatabase("DemoDB6").Options);
+            await context.Entities.InsertAsync(new List<Entity> { new Entity { Id = "1", Content = "a" }, new Entity { Id = "2", Content = "b" } });
+
+            // Act
+            await context.Entities.UpdateAsync(new Entity { Id = "1", Content = "c" });
+            var list = await context.Entities.OrderBy(e => e.Id).ToListAsync();
+
+            // Assert
+            Assert.Equal(2, list.Count);
+            Assert.Equal("1", list.First().Id);
+            Assert.Equal("2", list.Last().Id);
+            Assert.Equal("c", list.First().Content);
+            Assert.Equal("b", list.Last().Content);
+        }
+
+        [Fact]
+        public void DiscardChangesTest()
+        {
+            // Arrange
+            var context = new DatabaseContext(new DbContextOptionsBuilder<DatabaseContext>().UseInMemoryDatabase("DemoDB7").Options);
+
+            // Act
+            context.Entities.Add(new Entity { Id = "1", Content = "a" });
+            context.DiscardChanges();
+
+            // Assert
+            Assert.Empty(context.Entities);
         }
     }
 }
