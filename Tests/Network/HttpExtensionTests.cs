a8bbc3c7df1cd67a0b028c02f84a449ca7bb90fa
Kristof Papp <pappkristof1@gmail.com>
2021-12-29 15:19:57 +0100

    Remove reference

--- Tests/Network/HttpExtensionTests.cs
+++ Tests/Network/HttpExtensionTests.cs
@@ -4,7 +4,6 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Common.Application;
 using Common.Backend;
-using Common.Web;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
