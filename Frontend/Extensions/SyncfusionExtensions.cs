28560ff23bf5acec7853cda14041913c0d549975
Kristof Papp <pappkristof1@gmail.com>
2022-01-06 12:20:32 +0100

    Chart legend

---
+++ Frontend/Extensions/SyncfusionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Syncfusion.Blazor.Charts;
+
+namespace Common.Web
+{
+    public static class SyncfusionExtensions
+    {
+        public static string ToFormat(this RangeIntervalType intervalType)
+        {
+            return intervalType switch
+            {
+                RangeIntervalType.Auto => null,
+                RangeIntervalType.Years => "yyyy",
+                RangeIntervalType.Quarter => "yyyy.MMMM",
+                RangeIntervalType.Months => "MMMM",
+                RangeIntervalType.Weeks => "MMMM",
+                RangeIntervalType.Days => "MM.dd",
+                RangeIntervalType.Hours => "hh",
+                RangeIntervalType.Minutes => "hh.mm",
+                RangeIntervalType.Seconds => "mm.ss",
+                _ => throw new NotImplementedException(),
+            };
+        }
+    }
+}
