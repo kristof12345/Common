2012b8fa37ba143e392890a365db9ea54803b8f4
Kristof Papp <pappkristof1@gmail.com>
2022-01-28 16:08:13 +0100

    Tests for cache

--- Tests/Models/CommonModelTests.cs
+++ Tests/Models/CommonModelTests.cs
@@ -27,7 +27,7 @@ namespace Common.Tests.Models
         [Fact]
         public void ServerSettingsTest()
         {
-            var settings = new ServerSettings { BaseUrl = "url", Version="1.0", RenderMode="Server" };
+            var settings = new ServerSettings { BaseUrl = "url", Version = "1.0", RenderMode = "Server" };
             ServerSettings.Instance = settings;
 
             Assert.Equal("url", settings.BaseUrl);
@@ -39,7 +39,7 @@ namespace Common.Tests.Models
         [Fact]
         public void LoginRequestTest()
         {
-            var request = new LoginRequest { Username="username", Password="password" };
+            var request = new LoginRequest { Username = "username", Password = "password" };
 
             Assert.Equal("username@password", request.ToString());
         }
@@ -47,7 +47,7 @@ namespace Common.Tests.Models
         [Fact]
         public void PasswordRequestTest()
         {
-            var request = new ChangePasswordRequest { OldPassword = "oldpassword", NewPassword="newpassword" };
+            var request = new ChangePasswordRequest { OldPassword = "oldpassword", NewPassword = "newpassword" };
 
             Assert.Equal("oldpassword", request.OldPassword);
             Assert.Equal("newpassword", request.NewPassword);
@@ -67,6 +67,16 @@ namespace Common.Tests.Models
             Assert.Equal("db3OIsj+BXE9NZDy0t8W3TcNekrF+2d/1sFnWG4HnV8TZY30iTOdtVWJG8abWvB1GlOgJuQZdcF2Luqm/hccMw==", App.TokenSettings.Secret);
             Assert.Equal(3000, App.TokenSettings.ExpireMinutes);
             Assert.Null(App.Services.GetService<TimeService>());
+        }
+
+        [Fact]
+        public void AppUserTest()
+        {
+            var user = new AppUser { Id = "username", Name = new Name("User", "Name"), Image = "image" };
+
+            Assert.Equal("username", user.Id);
+            Assert.Equal("User Name", user.Name.ToString());
+            Assert.Equal("image", user.Image);
         }
     }
 }
--- Tests/Services/CacheServiceTests.cs
+++ Tests/Services/CacheServiceTests.cs
@@ -3,7 +3,7 @@ using Common.Application;
 using Common.Backend;
 using Xunit;
 
-namespace Common.Tests.Frontend
+namespace Common.Tests.Services
 {
     public class CacheServiceTests
     {
@@ -75,6 +75,46 @@ namespace Common.Tests.Frontend
 
             // Assert
             Assert.Null(cache.Get<string>("Key"));
+        }
+
+        [Fact]
+        public void ClearCategoryTest()
+        {
+            // Arrange
+            var cache = new CacheService();
+
+            // Act
+            cache.Set("Key1", "Value1", 1);
+            cache.Set("Key2", "Value2", 1);
+            cache.Set("Key3", "Value3", 2);
+            cache.Set("Key4", "Value4", 2);
+            cache.ClearCategory(2);
+
+            // Assert
+            Assert.NotNull(cache.Get<string>("Key1"));
+            Assert.Equal("Value2", cache.Get<string>("Key2"));
+            Assert.Null(cache.Get<string>("Key3"));
+            Assert.Null(cache.Get<string>("Key4"));
+        }
+
+        [Fact]
+        public void ClearEmptyCategoryTest()
+        {
+            // Arrange
+            var cache = new CacheService();
+
+            // Act
+            cache.Set("Key1", "Value1", 1);
+            cache.Set("Key2", "Value2", 1);
+            cache.Set("Key3", "Value3", 2);
+            cache.Set("Key4", "Value4", 2);
+            cache.ClearCategory(3);
+
+            // Assert
+            Assert.NotNull(cache.Get<string>("Key1"));
+            Assert.Equal("Value2", cache.Get<string>("Key2"));
+            Assert.NotNull(cache.Get<string>("Key3"));
+            Assert.Equal("Value4", cache.Get<string>("Key4"));
         }
     }
 }
