ad698fd23ad67061bd4837a01be4d078333415a7
Kristof Papp <pappkristof1@gmail.com>
2022-02-09 20:13:04 +0100

    Commit

---
+++ Backend/Attributes/AdministratorAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Common.Application;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Common.Backend.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public sealed class AdministratorAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            try
+            {
+                string token = context.HttpContext.Request.Headers["Authorization"];
+                var user = new TokenService().DecodeToken(token?[7..]);
+                if (user.Type != UserType.Admin) throw new AuthorizationException("Admin privilages required.");
+            }
+            catch (Exception)
+            {
+                context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+            }
+        }
+    }
+}
