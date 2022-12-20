3398c90ba3924f8ecaad54f39ca95c5275576532
Kristof Papp <pappkristof1@gmail.com>
2022-12-20 12:02:32 +0100

    User interface

---
+++ Application/Interfaces/User.cs
@@ -0,0 +1,20 @@
+﻿namespace Common.Application;
+
+public class IUser : IEntity<string>
+{
+    public virtual string Id { get; set; }
+
+    public virtual string Firstname { get; set; }
+
+    public virtual string Surname { get; set; }
+
+    public virtual string Password { get; set; }
+
+    public virtual string Email { get; set; }
+
+    public virtual UserType Type { get; set; }
+
+    public virtual string Image { get => $"Data/UserProfiles/{Id}.png"; }
+
+    public virtual string Name { get => Firstname + " " + Surname; }
+}
